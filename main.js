0ca784f7faaa06dcc3ddd7c69a19bd0ca262f6ae
2dotsighborg <wellsdey@gmail.com>
2018-03-21 23:34:00 +0100

    aforementioned expected update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $("#reg").click(function(){
         $(this).css("border-bottom", "1px solid rgba(0,0,255,1)");
         $("#log").css("border-bottom", "1px solid rgba(0,0,0,0.2)");
+        // $("#log:hover").css("border-bottom", "1px solid blue");
+        // $("#log").toggleClass("hovered");
         $("#flexi-1").css("display", "block");
         $("#flexi-2").css("display", "none");
     });
@@ -9,9 +11,20 @@ $(document).ready(function(){
     $("#log").click(function(){
         $(this).css("border-bottom", "1px solid rgba(0,0,255,1)");
         $("#reg").css("border-bottom", "1px solid rgba(0,0,0,0.2)");
+        // $("#reg:hover").css("border-bottom", "1px solid blue");
+        // $("#reg").toggleClass("hovered");
         $("#flexi-1").css("display", "none");
         $("#flexi-2").css("display", "block");
     });
 });
 
 // border-bottom: 1px solid blue;
+
+
+/* TO DO: fix hover states */
+// #reg:hover {
+//     border: none;
+//     border-bottom: 1px solid ;
+// }
+
+
